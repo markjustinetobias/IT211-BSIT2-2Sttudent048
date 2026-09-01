6f98f34ba00ac2b3d8282ca0a901ef36e1bb8326
Mark Justine Tobias <markjustinetobias19@gmail.com>
2026-09-01 19:17:46 +0800

    Add files via upload

---
+++ exercise2.java
@@ -0,0 +1,8 @@
+public class exercise2 {
+    public static void main(String[] args) {
+        int b = 8;
+        int h = 4;
+        int area = (b * h) / 2;
+        System.out.println("The area of the triangle is: " + area);
+    }
+}
---
+++ exercise4.java
@@ -0,0 +1,7 @@
+public class exercise4 {
+    public static void main(String[] args) {
+        double radius = 4.0;
+        double area = Math.PI * radius * radius;
+        System.out.println("The area of the circle is: " + area);
+    }
+}
